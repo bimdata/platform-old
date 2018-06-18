e35b36f6d743994034cfead0fb803dff8426019b
Samy <samy@mbp-de-samy.home>
2018-06-18 18:25:46 +0200

    Style for front

--- src/components/PageLoginSignUp.vue
+++ src/components/PageLoginSignUp.vue
@@ -3,6 +3,7 @@
         <b-card class="shadow-sm signup">
             <div class="login__head-login text-center">
                 <img class="login__logo" src="../assets/images/logo-bimdata-carre.svg" width="100" height="100"/>
+                <p class="login__baseline">{{ $t("message.login_baseline") }}</p>
             </div>
             <b-form @submit="handleSignUp">
                 <b-form-group id="signup-group-username"
