8fc2e2bf6e75f38641c81153ef4e5744cc79a67f
Amoki <hugo.duroux@gmail.com>
2018-06-29 15:31:51 +0200

    update production environment

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,9 +1,8 @@
 'use strict'
 const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
 
-module.exports = merge(prodEnv, {
+module.exports = {
   NODE_ENV: '"development"',
   BD_ENV: '"staging"',
   BD_API_BASE_URL: '"https://api-staging.bimdata.io/oauth/v2/"'
-})
+}
--- config/staging.env.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const bimEnv = require('./bim.env')
-
-module.exports = merge(bimEnv, {
-  NODE_ENV: '"development"',
-  BD_ENV: '"staging"',
-  BD_API_BASE_URL: '"https://api-staging.bimdata.io/oauth/v2/"'
-})
