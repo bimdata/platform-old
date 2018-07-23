3d21e842efec443559743198c234744e64500106
Samy <samy@bimdata.io>
2018-07-23 10:44:07 +0200

    Script for importing needed material svg icon in build folder

---
+++ build/get-material-icons.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const sourcePath = 'node_modules/@mdi/svg/svg';
+const destPath = 'src/assets/images/icons';
+
+const svgIcons = [
+  'chevron-down.svg',
+  'chevron-left.svg',
+  'chevron-right.svg',
+  'chevron-up.svg'
+];
+
+fs.readdir(sourcePath, function(err, items) {
+
+  for (let item of items) {
+    if (svgIcons.indexOf(item) != -1) {
+      try {
+        copyFile(sourcePath + '/' + item, destPath + '/' + item)
+          .then(() => { console.log(item + ' copied') })
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+});
+
+async function copyFile(source, target) {
+  var rd = fs.createReadStream(source);
+  var wr = fs.createWriteStream(target);
+  try {
+    return await new Promise(function(resolve, reject) {
+      rd.on('error', reject);
+      wr.on('error', reject);
+      wr.on('finish', resolve);
+      rd.pipe(wr);
+    });
+  } catch (error) {
+    rd.destroy();
+    wr.end();
+    throw error;
+  }
+}
