0b9b458d82b8bb2b663e843d8be5c67b611cc880
Samy <samy@bimdata.io>
2018-07-25 11:32:56 +0200

    Add base action button components

---
+++ src/components/base-components/BaseButtonAction.vue
@@ -0,0 +1,20 @@
+<template>
+    <b-btn class="base-button-action"
+            v-on="listeners">
+        <slot>Button</slot>
+    </b-btn>
+</template>
+<script>
+export default {
+  props: {
+    text: String
+  },
+  computed: {
+    listeners () {
+      return {
+        ...this.$listeners
+      }
+    }
+  }
+}
+</script>
