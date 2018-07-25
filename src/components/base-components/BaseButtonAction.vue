4ddc076c71c7349ea5689e31dcc221341cc1b517
Samy <samy@bimdata.io>
2018-07-25 14:21:16 +0200

    Button Action variant

--- src/components/base-components/BaseButtonAction.vue
+++ src/components/base-components/BaseButtonAction.vue
@@ -1,13 +1,21 @@
 <template>
     <b-btn class="base-button-action"
+           :class="{
+               'base-button-action--small': size === 'small',
+               'base-button-action--fullwidth': size === 'fullwidth',
+               'base-button-action--icon-right': iconPosition === 'right'
+           }"
             v-on="listeners">
-        <slot>Button</slot>
+        <svgicon class="base-button-action__icon" v-show="iconName !== undefined" :name="iconName" width="15"></svgicon>
+        <span><slot>Button</slot></span>
     </b-btn>
 </template>
 <script>
 export default {
   props: {
-    text: String
+    size: String,
+    iconPosition: String,
+    iconName: String
   },
   computed: {
     listeners () {
