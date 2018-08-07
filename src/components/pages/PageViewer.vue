dbfe39edad194e6ec52337760608de55633a161d
Samy <samy@bimdata.io>
2018-08-07 15:06:15 +0200

    iframe integration

--- src/components/pages/PageViewer.vue
+++ src/components/pages/PageViewer.vue
@@ -1,5 +1,21 @@
 <template>
     <div>
-        page viewer
+        <div id="embed" style="width: 100%; height: 500px;" ></div>
     </div>
 </template>
+<script>
+import scriptjs from 'scriptjs'
+
+export default {
+  created () {
+    scriptjs('https://cdn-beta.bimdata.io/js/bimdata-viewer-embed.js', () => {
+      window.BIMDataViewer('embed', {
+        accessToken: localStorage.getItem('token'),
+        cloudId: parseInt(this.$route.params.cloudId),
+        projectId: parseInt(this.$route.params.projectId),
+        ifcId: parseInt(this.$route.params.ifcId)
+      })
+    })
+  }
+}
+</script>
