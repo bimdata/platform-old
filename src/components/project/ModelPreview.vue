0c5d469f9f8267bf7d5cfa8a7dbc3bfbcac8d61c
Samy <samy@bimdata.io>
2018-09-12 15:34:52 +0200

    Model preview test

--- src/components/project/ModelPreview.vue
+++ src/components/project/ModelPreview.vue
@@ -1,18 +1,20 @@
 <template>
-  <div ref="div" class="model-preview"
-       @mousemove.stop="rotate"
-       @mouseleave.stop="leave"
-       >
-    <img ref="image" src="../../assets/images/model-preview.png"/>
+  <div ref="div" class="model-preview">
+    <div v-for="(value, index) in nbSlices"
+         :key="value"
+         @mousemove="rotate(index)"
+         class="slice">{{ index }}</div>
+    <img :style="{left: -(imagePositionState * 640) + 'px'}" ref="image" src="../../assets/images/model-preview.png"/>
+      {{ sliceCurrentIndex }}
   </div>
 </template>
 <script>
 export default {
   data () {
     return {
-      slice: 0,
-      startIndex: 0,
-      currentIndex: 0
+      nbSlices: 15,
+      sliceCurrentIndex: 0,
+      imagePositionState: 0
     }
   },
   props: {
@@ -23,22 +25,28 @@ export default {
     }
   },
   methods: {
-    leave (e) {
-      this.startIndex = this.currentIndex
-    },
+    rotate (index) {
+      let moving = index - this.sliceCurrentIndex
 
-    rotate (e) {
-      var x = e.clientX - this.$refs.div.getBoundingClientRect().left
-      var width = this.$refs.div.offsetWidth
-      this.slice = 14 - Math.trunc(x * 15 / width)
-      this.currentIndex = this.slice - this.startIndex
-      this.currentIndex = this.slice
-      if (this.currentIndex < 0) {
-        this.currentIndex = 13 + this.currentIndex
+      if (moving <= 1 && moving >= -1) {
+        this.imagePositionState += moving
+        this.imagePositionState %= this.nbSlices
+        if (this.imagePositionState < 0) {
+          this.imagePositionState += this.nbSlices
+        }
       }
-      var position = `${this.currentIndex * 640}px`
-      this.$refs.image.style.right = position
+      this.sliceCurrentIndex = index
     }
   }
 }
 </script>
+<style lang="scss">
+.model-preview {
+    display: flex;
+    position: relative;
+}
+.slice {
+    min-width: 6.6667%;
+    height: 500px;
+}
+</style>
