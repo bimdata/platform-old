5a58ef3ad7deab98d6e0b4d5e438e25439aa4d69
Jessyca HERVIEUX <jessybloom@hotmail.fr>
2019-01-14 15:10:55 +0100

    inte menu user

---
+++ src/components/base-components/BaseDropdown.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="base-dropdown">
+      <b-dropdown
+          :text="title"
+          size="lg"
+          id="user-menu__ddown"
+          class="user-menu__ddown"
+          right
+        >
+          <slot name="base-dropdown-menu">
+
+          </slot>
+        </b-dropdown>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
