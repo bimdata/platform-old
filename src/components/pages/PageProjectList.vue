66c117a5b3ba40de4f5dcdee13ab021fb3beb193
admin <admin@mbp-de-alex.home>
2019-01-14 09:20:12 +0100

    creation dossier qd on appuit sur netree page project-list

--- src/components/pages/PageProjectList.vue
+++ src/components/pages/PageProjectList.vue
@@ -10,46 +10,42 @@
                 </base-choice-list>
             </div>
         </div>
-        <!--<div class="row">-->
-            <!--<div class="col-12">-->
-                <transition-group name="project-item" tag="div" class="project_list row">
-                    <div class="card-container" key="new">
-                        <div class="base-card card-item project-item new-project-item">
-                        <div class="new-project-item__card-container" :class="{active: displayNewForm}">
-                            <div class="new-project-item__button-container new-project-item__container">
-                                <div class="new-project-item__button-new" @click="displayNewForm = true">
-                                    <svgicon name="plus" height="45" width="45" color="#FFF"></svgicon>
-                                </div>
-                                <p>{{ $t('project_list.new_project') }}</p>
-                            </div>
-                            <div class="new-project-item__edit-container new-project-item__container">
-                                <div class="new-project-item__edit-container__header">
-                                    <span>{{ $t('project_list.new_project') }}</span>
-                                    <div @click="displayNewForm = false">
-                                    <svgicon name="close"
-                                             height="20"
-                                             width="20">
-                                    </svgicon>
-                                    </div>
-                                </div>
-                                <div class="new-project-item__edit-container__edit-container__body">
-                                    <div class="base-input-text-material">
-                                        <input type="text" required v-model="newProjectName">
-                                        <span class="highlight"></span>
-                                        <span class="bar"></span>
-                                        <label>{{ $t('project_list.project_name') }}</label>
-                                    </div>                                </div>
-                                <div class="new-project-item__edit-container__edit-container__footer">
-                                    <base-button-action @click="createProject">{{ $t('project_list.submit') }}</base-button-action>
-                                </div>
+        <transition-group name="project-item" tag="div" class="project_list row">
+            <div class="card-container" key="new">
+                <div class="base-card card-item project-item new-project-item">
+                <div class="new-project-item__card-container" :class="{active: displayNewForm}">
+                    <div class="new-project-item__button-container new-project-item__container">
+                        <div class="new-project-item__button-new" @click="displayNewForm = true">
+                            <svgicon name="plus" height="45" width="45" color="#FFF"></svgicon>
+                        </div>
+                        <p>{{ $t('project_list.new_project') }}</p>
+                    </div>
+                    <div class="new-project-item__edit-container new-project-item__container">
+                        <div class="new-project-item__edit-container__header">
+                            <span>{{ $t('project_list.new_project') }}</span>
+                            <div @click="displayNewForm = false">
+                            <svgicon name="close"
+                                      height="20"
+                                      width="20">
+                            </svgicon>
                             </div>
                         </div>
+                        <div class="new-project-item__edit-container__edit-container__body">
+                            <div class="base-input-text-material">
+                                <input type="text" required v-model="newProjectName" v-on:keyup.enter="createProject">
+                                <span class="highlight"></span>
+                                <span class="bar"></span>
+                                <label>{{ $t('project_list.project_name') }}</label>
+                            </div>                                </div>
+                        <div class="new-project-item__edit-container__edit-container__footer">
+                            <base-button-action @click="createProject">{{ $t('project_list.submit') }}</base-button-action>
+                        </div>
                     </div>
-                    </div>
-                    <card-project-list v-for="(project) in getProjectByCloud()" :key="project.id" :project="project"></card-project-list>
-                </transition-group>
-            <!--</div>-->
-        <!--</div>-->
+                </div>
+            </div>
+            </div>
+            <card-project-list v-for="(project) in getProjectByCloud()" :key="project.id" :project="project"></card-project-list>
+        </transition-group>
     </div>
 </template>
 <script>
