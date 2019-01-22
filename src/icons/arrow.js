5ad899fea399be8a0d320e5cfe0e4591de799733
Jessyca HERVIEUX <jessybloom@hotmail.fr>
2019-01-22 10:50:18 +0100

    espaces picto arbo ged + ajout icone folder open

---
+++ src/icons/arrow.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+import * as icon from 'vue-svgicon'
+icon.register({
+  'arrow': {
+    width: 24,
+    height: 24,
+    viewBox: '0 0 24 24',
+    data: '<path pid="0" d="M10 17l5-5-5-5v10z"/><path pid="1" _fill="none" d="M0 24V0h24v24H0z"/>'
+  }
+})
---
+++ src/icons/folder-outline.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+import * as icon from 'vue-svgicon'
+icon.register({
+  'folder-outline': {
+    width: 24,
+    height: 24,
+    viewBox: '0 0 24 24',
+    data: '<path pid="0" d="M20 18H4V8h16m0-2h-8l-2-2H4c-1.11 0-2 .89-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8a2 2 0 0 0-2-2z"/>'
+  }
+})
