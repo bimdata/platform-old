b3d8f29bc77d6b1bdedd0ee3f1d11f2125ffc05f
Jessyca HERVIEUX <jessybloom@hotmail.fr>
2019-02-04 16:37:53 +0100

    ajout loader layout chargement page accueil

--- src/components/layouts/LayoutPageDashboard.vue
+++ src/components/layouts/LayoutPageDashboard.vue
@@ -4,14 +4,18 @@
         <article class="main">
           <div :class="this.$route.meta.container" class="h-100">
             <transition name="slide-fade" mode="out-in">
-                <router-view :key="$route.fullPath" v-if="initializedData"></router-view>
+                <router-view :key="$route.fullPath" v-if="displayPage"></router-view>
+                <div class="loader loader-layout" v-else>
+                  <p class="loader-layout__text">Loading</p>
+                  <div class="lds-dual-ring"></div>
+                </div>
             </transition>
           </div>
         </article>
     </div>
 </template>
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapState } from 'vuex'
 import MainHeader from '@/components/navigation/MainHeader'
 
 export default {
@@ -23,10 +27,20 @@ export default {
       initializedData: false
     }
   },
+  computed: {
+    ...mapState({
+      loaderPage: state => state.loaderPage
+    }),
+    displayPage () {
+      return this.initializedData && !this.loaderPage
+    }
+  },
   async created () {
     await this.init()
+    this.$store.commit('SET_LOADER_PAGE', true)
     await this.fetchUserCloudsDetails()
     this.initializedData = true
+    this.$store.commit('SET_LOADER_PAGE', false)
   },
   methods: {
     ...mapActions({
