ed7bcf837a5d9c9da5c4061039566e97324c4b3e
Vladislav Gritsenko <Vladislav.Gritsenko@noveogroup.com>
2019-02-05 13:22:14 +0300

    Load custom viewer url from store

--- src/components/pages/PageViewer.vue
+++ src/components/pages/PageViewer.vue
@@ -1,26 +1,38 @@
 <template>
     <div class="h-100">
         <div v-if="!customViewer" id="embed" style="width: 100%; height: 100%;" ></div>
-        <iframe v-if="customViewer" :src="customUrl" width="100%" height="100%"></iframe>
+        <iframe v-if="customViewer" :src="customUrl" width="100%" height="100%" class="no-borders"></iframe>
     </div>
 </template>
 <script>
 import scriptjs from 'scriptjs'
-import { mapGetters } from 'vuex'
+import { mapGetters, mapMutations } from 'vuex'
 
 export default {
   data () {
     return {
-      customViewer: false
+      customUrl: null
     }
   },
   computed: {
-    ...mapGetters(['oidcAccessToken', 'getCustomUrl'])
+    ...mapGetters(['oidcAccessToken', 'getCloudById', 'getCustomUrl']),
+    customViewer () {
+      return !!this.getCustomUrl && this.customUrl
+    }
+  },
+  methods: {
+    ...mapMutations({
+      mutateCurrentCloud: 'SET_CURRENT_CLOUD'
+    }),
+    setCurrentCloud () {
+      const cloud = this.getCloudById(this.$route.params.cloudId)
+      this.mutateCurrentCloud(cloud)
+    }
   },
   created () {
+    this.setCurrentCloud()
     const params = this.$route.params
     if (this.getCustomUrl) {
-      this.customViewer = true
       this.customUrl = `${this.getCustomUrl}/?cloudId=${params.cloudId}&projectId=${params.projectId}&ifcId=${params.ifcId}&accessToken=${this.oidcAccessToken}`
     } else {
       scriptjs(`${process.env.BD_CDN_BASE_URL}/js/bimdata-viewer-embed.js`, () => {
