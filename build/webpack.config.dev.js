9f0a98d7830d13b8f51dff0a626413b7dee68cc6
Vladislav Gritsenko <Vladislav.Gritsenko@noveogroup.com>
2019-02-08 18:05:23 +0300

    More fixes for build

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.config.base')
+
+const HOST = 'localhost'
+const PORT = 8080
+
+module.exports = merge(baseConfig, {
+  mode: 'development',
+
+  devServer: {
+    historyApiFallback: true,
+    clientLogLevel: 'warning',
+    hot: true,
+    contentBase: 'dist',
+    compress: true,
+    host: HOST,
+    port: PORT,
+    open: true,
+    overlay: { warnings: false, errors: true },
+    publicPath: '/',
+    quiet: true,
+    watchOptions: {
+      poll: true
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }, {
+        test: /\.scss?$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+})
