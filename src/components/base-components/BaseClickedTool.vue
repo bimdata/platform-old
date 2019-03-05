8bc8bf266ef134f6dddbde2d0823f6102b4584ad
Jessyca HERVIEUX <jessybloom@hotmail.fr>
2019-03-05 12:23:54 +0100

    creation composant baseClickedTool

---
+++ src/components/base-components/BaseClickedTool.vue
@@ -0,0 +1,41 @@
+<template>
+  <span class="base-button-option__tool" @click="clickedTool" :class="{clicked: clicked}">
+    <svgicon :name="iconName" :width="iconWidth" :height="iconHeight" @click.native="clickedToolEvent" :class="iconName"></svgicon>
+  </span>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clicked: false
+    }
+  },
+  props: {
+    iconName: {
+      type: String,
+      default: ''
+    },
+    iconHeight: {
+      type: String,
+      default: '22'
+    },
+    iconWidth: {
+      type: String,
+      default: '22'
+    }
+  },
+  methods: {
+    clickedTool () {
+      this.clicked = false
+      this.clicked = true
+      setTimeout(() => {
+        this.clicked = false
+      }, 500)
+    },
+    clickedToolEvent () {
+      this.$emit('on-clicked-tool')
+    }
+  }
+}
+</script>
