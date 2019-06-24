03d0a67bd62e88ca838e75b9243c549b8dbbbfbb
benjaminaudet <benjamin.audet@epitech.eu>
2019-06-24 18:14:31 +0200

    Remove button when it's a folder

--- src/components/project/DMS.vue
+++ src/components/project/DMS.vue
@@ -157,7 +157,7 @@
                         <svgicon name="play" width="13" height="13"></svgicon>
                         {{ $t('project.view') }}
                       </li>
-                      <li @click="downloadFile(documentAction)">
+                      <li @click="downloadFile(documentAction)" v-if="documentAction.item.type != 'Folder'">
                         <svgicon name="download" width="13" height="13"></svgicon>
                           {{ $t('project.download') }}
                       </li>
@@ -365,6 +365,7 @@ export default {
       }
     },
     downloadFile (documentAction) {
+      console.log(documentAction)
       window.open(documentAction.item.file)
     },
     formatCreatorCell (creator) {
