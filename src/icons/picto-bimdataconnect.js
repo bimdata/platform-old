f4592a81da84ea457b299db56b3b09d31fa67a5e
Gaelle <gaelle@bimdata.io>
2019-07-11 10:01:09 +0200

    add picto BIMData Connect

---
+++ src/icons/picto-bimdataconnect.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'picto-bimdataconnect': {
+    width: 16,
+    height: 16,
+    viewBox: '0 0 23 23',
+    data: '<path pid="0" d="M13 5.1V1L6.7 5v4.1l-1.3.9v9.2h3.5v2h7.7V2.8L13 5.1zm0 14.1V6.5l2.5-1.6V20h-5.4v-.9H13zM7.8 5.7l4-2.6v2.7l-4 2.6V5.7zm4 1.5V18H6.6v-7.4l5.2-3.4z" _fill="#30374b"/>'
+  }
+})
