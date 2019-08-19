552bee02400f01e3ef9412c71dab41eeaa2c6a6a
Amoki <hugo.duroux@gmail.com>
2019-08-19 15:22:45 +0200

    back to Bearer header

--- src/api/initClient.js
+++ src/api/initClient.js
@@ -3,10 +3,9 @@ import * as bimdata from '@bimdata/bimdata-api-client'
 export const generateClient = (accessToken) => {
   const defaultClientInstance = bimdata.ApiClient.instance
   defaultClientInstance.basePath = process.env.BD_API_BASE_URL
-  let Bearer = defaultClientInstance.authentications.Bearer
-  Bearer.apiKeyPrefix = 'JWT'
-  Bearer.type = 'apiKey'
-  Bearer.apiKey = accessToken
+  let bearer = defaultClientInstance.authentications.Bearer
+  bearer.type = 'oauth2'
+  bearer.accessToken = accessToken
 
   return bimdata
 }
