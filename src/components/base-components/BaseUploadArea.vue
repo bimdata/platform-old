996eae8ff02dcd0c47204d4b2ceb68ef252580cc
benjaminaudet <benjamin.audet@epitech.eu>
2019-08-20 17:16:26 +0200

    Update Base Upload Area component to avoid using Modal and implement drag'n'drop

--- src/components/base-components/BaseUploadArea.vue
+++ src/components/base-components/BaseUploadArea.vue
@@ -1,13 +1,11 @@
 <template>
     <div class="upload-area">
-        <button class="uppy UppyModalOpenerBtn" style="display: none;">Open Uppy Dashboard Modal</button>
-        <div class="DashboardContainer"></div>
     </div>
 </template>
 <script>
 import Uppy from '@uppy/core'
-import Dashboard from '@uppy/dashboard'
 import XHRUpload from '@uppy/xhr-upload'
+import DragDrop from '@uppy/drag-drop'
 
 export default {
   props: {
@@ -28,11 +26,10 @@ export default {
     let baseApiUrl = process.env.BD_API_BASE_URL
     let endpointUpload = baseApiUrl + '/cloud/' + this.cloudId + '/project/' + this.projectId + '/document'
     let token = this.$store.state.oidc.access_token
-    let target = this.target
 
     const uppy = new Uppy({
       debug: false,
-      autoProceed: false,
+      autoProceed: true,
       restrictions: {
         maxFileSize: 1000000000, // 1 Go
         maxNumberOfFiles: null,
@@ -40,16 +37,12 @@ export default {
         allowedFileTypes: ['.ifc', '.ifczip']
       }
     })
-      .use(Dashboard, {
-        trigger: '.UppyModalOpenerBtn',
-        inline: true,
-        target: target,
-        replaceTargetContent: true,
-        showProgressDetails: true,
-        note: '',
-        proudlyDisplayPoweredByUppy: false,
-        height: 163,
-        browserBackButtonClose: true
+      .use(DragDrop, {
+        target: '.upload-area',
+        width: '100%',
+        height: '100%',
+        note: null,
+        locale: {}
       })
       .use(XHRUpload, {
         endpoint: endpointUpload,
@@ -61,7 +54,6 @@ export default {
 
     uppy.on('complete', result => {
       setTimeout(() => {
-        uppy.reset()
         this.$emit('upload-complete', result)
       }, 2000)
     })
