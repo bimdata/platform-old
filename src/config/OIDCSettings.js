f595374e4032d7b3491834569de08c13427851f4
benjaminaudet <benjamin.audet@epitech.eu>
2019-09-17 15:17:06 +0200

    dqf

--- src/config/OIDCSettings.js
+++ src/config/OIDCSettings.js
@@ -3,7 +3,11 @@ export const oidcSettings = {
   client_id: process.env.BD_OIDC_CLIENT_ID,
   redirect_uri: process.env.BD_APP_URL + '/oidc-callback',
   post_logout_redirect_uri: process.env.BD_APP_URL,
+<<<<<<< Updated upstream
   response_type: 'code id_token token',
+=======
+  response_type: 'code',
+>>>>>>> Stashed changes
   scope: 'openid profile email',
   automaticSilentRenew: true,
   silent_redirect_uri: process.env.BD_APP_URL + '/oidc-callback',
