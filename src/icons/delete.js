e6c10ae4eb539b3dd5a3ad14b097b3ccf0f7d855
Gaelle <gaelle@bimdata.io>
2019-12-03 16:19:00 +0100

    delete old icons

--- src/icons/delete.js
+++ src/icons/delete.js
@@ -7,6 +7,6 @@ icon.register({
     width: 24,
     height: 24,
     viewBox: '0 0 24 24',
-    data: '<path pid="0" d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12z"/>'
+    data: '<path pid="0" d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 002 2h8a2 2 0 002-2V7H6v12z"/>'
   }
 })
--- src/icons/folder-outline.js
+++ src/icons/folder-outline.js
@@ -7,6 +7,6 @@ icon.register({
     width: 24,
     height: 24,
     viewBox: '0 0 24 24',
-    data: '<path pid="0" d="M20 18H4V8h16m0-2h-8l-2-2H4c-1.11 0-2 .89-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8a2 2 0 0 0-2-2z"/>'
+    data: '<path pid="0" d="M20 18H4V8h16m0-2h-8l-2-2H4c-1.11 0-2 .89-2 2v12a2 2 0 002 2h16a2 2 0 002-2V8a2 2 0 00-2-2z"/>'
   }
 })
