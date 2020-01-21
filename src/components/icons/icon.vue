d4ed3de9dae9494be7ec9853f7d3d7d28ebd6c84
Gaelle <gaelle@bimdata.io>
2020-01-21 18:42:07 +0100

    add template vue for icons

---
+++ src/components/icons/icon.vue
@@ -0,0 +1,49 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    :viewBox="viewBox"
+    :aria-labelledby="iconName"
+    :fill="iconColor"
+    role="presentation"
+  >
+    <slot />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    iconName: {
+      type: String,
+      default: 'box'
+    },
+    width: {
+      type: [Number, String],
+      default: 20
+    },
+    height: {
+      type: [Number, String],
+      default: 20
+    },
+    iconColor: {
+      type: String,
+      default: 'currentColor'
+    },
+    x: {
+      type: [Number, String],
+      default: 20
+    },
+    y: {
+      type: [Number, String],
+      default: 20
+    }
+  },
+  computed: {
+    viewBox () {
+      return `0 0 ${this.x} ${this.y}`
+    }
+  }
+}
+</script>
