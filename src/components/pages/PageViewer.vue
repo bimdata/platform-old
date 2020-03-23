717f96a0927e43962eb4c813e01310f947be171f
amoki <hugo@bimdata.io>
2020-03-23 12:45:32 +0100

    add bimAndCo plugin

--- src/components/pages/PageViewer.vue
+++ src/components/pages/PageViewer.vue
@@ -18,10 +18,12 @@ import { mapGetters, mapMutations } from 'vuex'
 import BimdataViewer from '@bimdata/viewer'
 import bimObjectPlugin from '@bimdata/bimobject-viewer-plugin'
 import gltfExtractorPlugin from '@bimdata/gltf-extractor-viewer-plugin'
+import bimAndCoPlugin from '@bimdata/bim-and-co-viewer-plugin'
 
 const availablePlugins = {
   bimobject: bimObjectPlugin,
-  gltfExtractor: gltfExtractorPlugin
+  gltfExtractor: gltfExtractorPlugin,
+  bimAndCo: bimAndCoPlugin
 }
 
 export default {
